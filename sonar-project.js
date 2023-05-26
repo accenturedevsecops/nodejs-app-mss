ef205c9fab92a368c3e701de1b0025285e7ac48c
raghu2671 <125441353+raghu2671@users.noreply.github.com>
2023-05-26 11:05:26 +0530

    Update sonar-project.js
    updaed publicip to private ip

--- sonar-project.js
+++ sonar-project.js
@@ -1,7 +1,7 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://65.2.170.68:9000/',
+  serverUrl: 'http://172.31.34.206:9000/',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
